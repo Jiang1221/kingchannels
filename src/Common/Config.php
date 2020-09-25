683fa5d2fa0dbcf1590b76a738011228443a4f01
蒋治彬 <Jiangzb@kingchannerls.com>
2020-09-25 15:53:35 +0800

    调整配置信息

--- src/Common/Config.php
+++ src/Common/Config.php
@@ -4,11 +4,11 @@ namespace Jzb\Common;
 class Config{
 
     public static $appInfo = [
-        'appId' => '5', // 您的应用Id
-        'appSecret' => 'yuhangxui6fsdalj7', // 您的应用秘钥
+        'appId' => '', // 您的应用Id
+        'appSecret' => '', // 您的应用秘钥
         'apiVersion' => '1.0', // 接口版本号
         'appVersion' => '1.0.0', // 应用版本号
-        'gatewayUrl' => 'https://topgateway.keledge.com/transfer', // 网关地址
+        'gatewayUrl' => '', // 网关地址
     ];
 
 }
